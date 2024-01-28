39f9d8837abb6a5c1518f7cc96457566281b0c0f
trevoralfstad <trevor@diveclass.com>
2024-01-28 11:38:43 -0800

    fixes bash deploy

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 # build our images
-docker build -t talfstad/multi-client:latest -t talfstad/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t talfstad/multi-server:latest -t talfstad/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t talfstad/multi-worker:latest -f -t talfstad/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t talfstad/multi-client-k8s:latest -t talfstad/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t talfstad/multi-server-k8s:latest -t talfstad/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t talfstad/multi-worker-k8s:latest -t talfstad/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
 # push up to docker hub w/ latest tag
-docker push talfstad/multi-client:latest
-docker push talfstad/multi-server:latest
-docker push talfstad/multi-worker:latest
+docker push talfstad/multi-client-k8s:latest
+docker push talfstad/multi-server-k8s:latest
+docker push talfstad/multi-worker-k8s:latest
 # push up to docker hub with SHA tag
-docker push talfstad/multi-client:$SHA
-docker push talfstad/multi-server:$SHA
-docker push talfstad/multi-worker:$SHA
+docker push talfstad/multi-client-k8s:$SHA
+docker push talfstad/multi-server-k8s:$SHA
+docker push talfstad/multi-worker-k8s:$SHA
 
 # deploy what we built build everything to google cloud
 # goes out to docker and pulls down images we just pushed
-kubectl apply -f k8s # apply k8s config files
+kubectl apply -Rf k8s # apply k8s config files
 
 # tell GCE to to update the image to <image-name>:latest
-kubectl set image deployments/client-deployment client=talfstad@multi-client:$SHA
-kubectl set image deployments/server-deployment server=talfstad@multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=talfstad@multi-worker:$SHA
+kubectl set image deployments/client-deployment client=talfstad/multi-client-k8s:$SHA
+kubectl set image deployments/server-deployment server=talfstad/multi-server-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=talfstad/multi-worker-k8s:$SHA
